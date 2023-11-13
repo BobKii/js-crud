2fd023de3b789cda78ad5567d3a23e0e47107c6b
BobKii <two.heap2008@gmail.com>
2023-11-13 23:34:10 +0200

    131123 day-1_PrePaid js-crud_2-product

--- src/route/index.js
+++ src/route/index.js
@@ -58,6 +58,62 @@ class User {
   };
 }
 
+// ======================  2 - product  ==========================================
+
+class Product {
+  constructor(name, price, description) {
+    this.id = Math.round(Math.random() * 1000000);
+    this.createDate = new Date();
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    return this;
+  }
+
+  static #list = [];
+
+  static getList = () => this.#list;
+
+  static add = (product) => {
+    this.#list.push(product);
+  };
+
+  static getById = (id) => {
+    this.#list.find((product) => product.id === id);
+    console.log(product.id);
+  };
+
+  static updateById = (id, data) => {
+    const product = this.getById(id);
+    if (product) {
+      this.update(product, data);
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  static update = (product, data) => {
+    if (data) {
+      // const index = this.#list.findIndex((id) => user.id === id);
+      product.price = data.price;
+      product.name = data.name;
+      product.description = data.description;
+    }
+  };
+
+  static deleteById = (id) => {
+    const index = this.#list.findIndex((product) => product.id === id);
+
+    if (index !== -1) {
+      this.#list.splice(index, 1);
+      return true;
+    } else {
+      return false;
+    }
+  };
+}
+
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
@@ -149,6 +205,96 @@ router.post("/user-update", function (req, res) {
     info: result ? "пошта email оновлена" : "Сталася поМилКа !!!",
   });
   // ↑↑ сюди вводимо JSON дані
+});
+
+// ======================  2 - product  ==========================================
+
+router.get("/product-create", function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  const list = Product.getList();
+
+  const { id, createDate, name, price, description } = req.body;
+
+  // const product = Product.getById(Number(id));
+
+  // if (user.verifyPassword(password)) {
+  //   User.update(user, { email });
+  //   result = true;
+  // }
+
+  // console.log(email, password, id);
+
+  // result = User.updateById(Number(id), { email });
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render("product-create", {
+    style: "product-create",
+
+    data: {
+      products: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
+    // info: "Товар створений",
+  });
+  // ↑↑ сюди вводимо JSON дані
+  console.log(data);
+});
+
+router.post("/product-create", function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  const { id, createDate, name, price, description } = req.body;
+  console.log(req.body);
+
+  const product = new Product(name, price, description);
+  Product.add(product);
+  console.log(User.getList());
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render("alert", {
+    style: "alert",
+    info: ["Товар створений", true],
+  });
+  // ↑↑ сюди вводимо JSON дані
+});
+
+router.get("/product-list", function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  const list = Product.getList();
+
+  const { id, createDate, name, price, description } = req.body;
+
+  // const product = Product.getById(Number(id));
+
+  // if (user.verifyPassword(password)) {
+  //   User.update(user, { email });
+  //   result = true;
+  // }
+
+  console.log(`Це list ---> ${{ list }}`);
+
+  // result = User.updateById(Number(id), { email });
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render("product-list", {
+    style: "product-list",
+
+    data: {
+      products: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
+    // info: "Товар створений",
+  });
+  // ↑↑ сюди вводимо JSON дані
+  // console.log("====== Це data =======");
+  // console.log(data);
+  // console.log("====== Це data =======");
 });
 
 // Підключаємо роутер до бек-енду
